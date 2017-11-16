0ec1e2d9909a0e127f607864d8215af22abba2aa
Валерійка <kudravka13@gmail.com>
2017-11-16 23:20:24 +0200

    reformated code

--- source-js/main.js
+++ source-js/main.js
@@ -1,87 +1,92 @@
 var input = document.getElementById("input-form"),
     list = document.getElementById("list_todo");
-    li = document.getElementsByTagName("li");
-document.getElementById("add").addEventListener("click", function (event) {
+
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
     add_item(event);
-})
+});
 
 document.getElementById("btn_all").addEventListener("click", function (event) {
-    document.querySelectorAll('#todo').forEach(function (elem) {
-        elem.classList.remove('hidden')
+    document.querySelectorAll("#todo").forEach(function (elem) {
+        elem.classList.remove("hidden");
     });
-    document.querySelectorAll('#complete').forEach(function (elem) {
-        elem.classList.remove('hidden')
+    document.querySelectorAll("#complete").forEach(function (elem) {
+        elem.classList.remove("hidden");
     });
-})
-
+});
 
 document.getElementById("btn_complete").addEventListener("click", function (event) {
-    document.querySelectorAll('#todo').forEach(function (elem) {
-        elem.classList.add('hidden')
+    document.querySelectorAll("#todo").forEach(function (elem) {
+        elem.classList.add("hidden");
     });
-    document.querySelectorAll('#complete').forEach(function (elem) {
-        elem.classList.remove('hidden')
+    document.querySelectorAll("#complete").forEach(function (elem) {
+        elem.classList.remove("hidden");
     });
-})
+});
 
 document.getElementById("btn_uncomplete").addEventListener("click", function (event) {
     document.querySelectorAll('#complete').forEach(function (elem) {
-        elem.classList.add('hidden')
+        elem.classList.add("hidden");
     });
-    document.querySelectorAll('#todo').forEach(function (elem) {
-        elem.classList.remove('hidden')
+    document.querySelectorAll("#todo").forEach(function (elem) {
+        elem.classList.remove("hidden");
     });
-})
+});
 
 function add_item(event) {
 
     if (input.value !== "") {
+//create element for list
         var newItem_li = document.createElement("li");
-        newItem_li.id = "todo";
         var newItem_p = document.createElement("p");
         var textnode = document.createTextNode(input.value);
+        input.value = '';
+
+// add element for list
+        newItem_li.id = "todo";
         newItem_p.appendChild(textnode);
         newItem_li.appendChild(newItem_p);
         list.insertBefore(newItem_li, list.childNodes[0]);
 
-
-//buttons
-        var buttons = document.createElement('div');
-        buttons.classList.add('buttons');
+//create buttons wraper
+        var buttons = document.createElement("div");
+        buttons.classList.add("buttons");
         newItem_li.appendChild(buttons);
 
 
-//complete
-        var complete = document.createElement('a');
-        complete.classList.add('btn-floating');
-        complete.classList.add('btn-small');
+//create complete button
+        var complete = document.createElement("a");
+        complete.classList.add("btn-floating");
+        complete.classList.add("btn-small");
+        complete.classList.add("waves-effect");
+        complete.classList.add("waves-light");
         complete.innerHTML = ' <i class="material-icons small ">done</i>';
-        complete.classList.add('complete');
+        complete.classList.add("complete");
         buttons.appendChild(complete);
-        complete.addEventListener('click', function () {
+
+//create complete listener
+        complete.addEventListener("click", function () {
             var item = this.parentNode.parentNode;
             var parent = item.parentNode;
             var id = parent.id;
-            item.id = (item.id === 'todo') ? 'complete' : 'todo';
+            item.id = (item.id === "todo") ? "complete" : "todo";
         });
 
 
-        //remove
-        var remove = document.createElement('a');
-        remove.classList.add('remove');
-        remove.classList.add('btn-floating');
-        remove.classList.add('btn-small');
+//create remove button
+        var remove = document.createElement("a");
+        remove.classList.add("remove");
+        remove.classList.add("btn-floating");
+        remove.classList.add("btn-small");
         remove.innerHTML = ' <i class="material-icons small ">clear</i>';
         buttons.appendChild(remove);
-        remove.addEventListener('click', function () {
 
+//create remove listener
+        remove.addEventListener("click", function () {
             var item = this.parentNode.parentNode;
             var parent = item.parentNode;
             parent.removeChild(item);
         });
-
-
-        input.value = '';
     }
 }
 
